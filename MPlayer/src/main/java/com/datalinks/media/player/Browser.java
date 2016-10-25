93d65b54c8280b3c1dffc049c6afc24d6acbe846
chris vugrinec <chris@macbook-air-chris.local>
2016-10-25 11:02:17 +0200

    added docker stuff

--- MPlayer/src/main/java/com/datalinks/media/player/Browser.java
+++ MPlayer/src/main/java/com/datalinks/media/player/Browser.java
@@ -1,12 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package com.datalinks.media;
+package com.datalinks.media.player;
 
+import com.datalinks.media.backend.MediaBackendServices;
 import java.net.URISyntaxException;
-import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.geometry.HPos;
 import javafx.scene.control.ComboBox;
 import javafx.scene.layout.HBox;
@@ -14,8 +15,12 @@ import javafx.stage.Stage;
 import javafx.scene.layout.Region;
 import javafx.geometry.Pos;
 import javafx.geometry.VPos;
+import javafx.scene.control.Hyperlink;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
+import javafx.scene.web.WebEvent;
+import javafx.scene.web.WebErrorEvent;
+
 
 /**
  *
@@ -23,14 +28,29 @@ import javafx.scene.web.WebView;
  */
 public class Browser extends Region {
 
-    final ComboBox comboBox = new ComboBox();
+    final static Logger logger = Logger.getLogger("Browser");
+    final static String USER_AGENTS = "Safari/537.44 Mozilla/5.0 AppleWebKit/537.44 JavaFX/8.0";
+
+    MediaBackendServices mbes = new MediaBackendServices();
+    ObservableList<Hyperlink> options = FXCollections.observableArrayList();
+    
+            
+    final ComboBox comboBox;
     final WebView browser = new WebView();
     final WebEngine webEngine;
     private final HBox toolBar;
     
     public Browser(Stage stage) throws URISyntaxException {
+
         
+        this.comboBox = new ComboBox(options);
         this.webEngine = browser.getEngine();
+                
+        comboBox.getSelectionModel().selectedItemProperty().addListener((ObservableValue ov, Object t, Object t1) -> {
+            logger.log(Level.INFO,"Changed to : {0}",t1.toString());
+        });
+        
+        
        //apply the styles
         getStyleClass().add("browser");
 
@@ -42,11 +62,43 @@ public class Browser extends Region {
 
         toolBar.getStyleClass().add("browser-toolbar");
         toolBar.getChildren().add(comboBox);
-     
-        webEngine.load(this.getClass().getResource("/azure-demo.html").toExternalForm());
+
+        String srcBrowser = this.getClass().getResource("/azure-internal.html").toExternalForm();
+
+        //  TODO: Hier nog mee experimeteren
+        webEngine.setUserAgent(USER_AGENTS);
+        webEngine.setJavaScriptEnabled(true);
+        
+        webEngine.setOnAlert((WebEvent<String> arg0) -> {
+            logger.log(Level.INFO,"Oeps: {0}",arg0.toString());
+        });
+        
+        webEngine.setOnError((WebErrorEvent t) -> {
+            logger.log(Level.SEVERE,"Error occured: {0}",t.getMessage());
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        });
+
+        //  Info
+        logger.log(Level.INFO, "Agent: {0}", webEngine.userAgentProperty().getValue());
+        logger.log(Level.INFO, "File: {0}", srcBrowser);
+        
+        //  Loading initial page
+        webEngine.load(srcBrowser);
         
         getChildren().add(toolBar);
         getChildren().add(browser);
+
+        //  Populating Links with stuff from service
+        mbes.getMediaItems().forEach((mediaItem) -> {
+            Hyperlink link = new Hyperlink();
+            link.setText(mediaItem.getName());
+            logger.log(Level.INFO,"Populating : {0} with link {1}",new Object[]{mediaItem.getName(),mediaItem.getLink()});
+            link.setOnAction((ActionEvent e) -> {
+                logger.log(Level.INFO,"should be redirecting to : {0}",mediaItem.getLink());
+                webEngine.load(mediaItem.getLink());
+            });
+            options.add(link);
+        });
 
     }
     
--- MPlayer/src/main/java/com/datalinks/media/player/Mplayer.java
+++ MPlayer/src/main/java/com/datalinks/media/player/Mplayer.java
@@ -1,38 +1,9 @@
-package com.datalinks.media;
+package com.datalinks.media.player;
 
 import javafx.application.Application;
-import javafx.application.Platform;
-import javafx.beans.value.ObservableValue;
-import javafx.collections.ListChangeListener.Change;
-import javafx.concurrent.Worker.State;
-import javafx.event.ActionEvent;
-import javafx.event.Event;
-import javafx.geometry.HPos;
-import javafx.geometry.Pos;
-import javafx.geometry.VPos;
-import javafx.print.PrinterJob;
-import javafx.scene.Node;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.ContextMenu;
-import javafx.scene.control.Hyperlink;
-import javafx.scene.control.MenuItem;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.input.MouseButton;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Priority;
-import javafx.scene.layout.Region;
 import javafx.scene.paint.Color;
-import javafx.scene.web.PopupFeatures;
-import javafx.scene.web.WebEngine;
-import javafx.scene.web.WebHistory;
-import javafx.scene.web.WebHistory.Entry;
-import javafx.scene.web.WebView;
 import javafx.stage.Stage;
-import netscape.javascript.JSObject;
 
 
 
